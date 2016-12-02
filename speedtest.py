9bcd2137a0f1735b604ca65f87e0b360ca88da13
jgte <j.g.deteixeiradaencarnacao@tudelft.nl>
2016-12-02 12:37:49 -0600

    substantial updated

--- speedtest.py
+++ speedtest.py
@@ -5,13 +5,27 @@ import re
 import time
 import sys
 
-def grep_it(pattern,string,default,debug):
+def grep_it(pattern,string,default,debug,show_error=True):
   if debug: print "pattern: <%s>" % pattern
   out = re.search(pattern,string)
-  if out:  out = out.group(1)
-  else:    out = default
+  try:
+    out = out.group(1)
+  except:
+    out = error_it(pattern,string,default,show_error)
   if debug: print "out: %s" % out
   return out
+
+def error_it(pattern,string,default,show_error=True):
+    if show_error:
+      print 'ERROR: failed to find pattern <'+pattern+'> in the following string:'
+      print string
+      print 'ERROR:',str(sys.exc_info())
+      print 'ERROR: using default value: '+default
+    return default
+
+def clean_it(string):
+  return string.replace('(','\(').replace(')','\)').replace('|','\|').replace('.','\.')
+
 
 debug = False
 
@@ -34,7 +48,11 @@ else:
   if len(sys.argv)>2:
     with open(sys.argv[2], 'r') as myfile: stats=myfile.read()
   else:
-    stats = subprocess.check_output(['/usr/local/bin/speedtest-cli','--share'])
+    try:
+      stats = subprocess.check_output(['/usr/local/bin/speedtest-cli','--share'])
+    except Exception as e:
+      sys.exit("ERROR: failed to call speedtest-cli: "+str(e))
+
 
 if debug: print stats
 
@@ -43,24 +61,24 @@ if debug: print stats
 # Upload: 5.29 Mbits/s
 
 pattern='Date: (.+?)\n'
-timestamp = grep_it(pattern,stats,time.strftime("%a, %d %b %Y %H:%M:%S %z (%Z)"),debug)
+timestamp = grep_it(pattern,stats,time.strftime("%a, %d %b %Y %H:%M:%S %z (%Z)"),debug,False)
 
 pattern='Testing from (.+?) \('
 testingfrom = grep_it(pattern,stats,'ERROR',debug)
 
-pattern='Testing from %s \((.+?)\)' % testingfrom
+pattern='Testing from %s \((.+?)\)' % clean_it(testingfrom)
 ip = grep_it(pattern,stats,'ERROR',debug)
 
 pattern='Hosted by (.+?) \['
 host = grep_it(pattern,stats,'ERROR',debug)
 
-pattern='Hosted by %s \[(.+?)\]:' % host.replace('(','\(').replace(')','\)')
+pattern='Hosted by %s \[(.+?)\]:' % clean_it(host)
 distance = grep_it(pattern,stats,'ERROR',debug)
 
 try:
-  pattern='Hosted by %s \[%s\]: (.+?)\n' % (host.replace('(','\(').replace(')','\)'),distance.replace('.','\.'))
-except AttributeError:
-  ping = 'ERROR'
+  pattern='Hosted by %s \[%s\]: (.+?)\n' % (clean_it(host),clean_it(distance))
+except:
+  ping = error_it(pattern,stats,'ERROR')
 else:
   ping = grep_it(pattern,stats,'ERROR',debug)
 
